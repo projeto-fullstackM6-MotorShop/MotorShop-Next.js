0eba44dd836677376c1f37d037b0e01e1f00e268
Victoria <victoria-milan@outlook.com>
2023-04-18 19:45:09 -0300

    feat/create fipe price field logic

--- motorshop/src/components/createAnnouncementModal.tsx
+++ motorshop/src/components/createAnnouncementModal.tsx
@@ -1,5 +1,6 @@
 import {
   Box,
+  Button,
   FormControl,
   FormLabel,
   Heading,
@@ -7,26 +8,37 @@ import {
   Select,
   TagLabel,
   Text,
+  Textarea,
 } from "@chakra-ui/react";
 import GeneralModal from "./generalModal";
 import { useAnnouncement } from "@/contexts/announcementContext";
 import { useState } from "react";
 import { ICarBrand } from "@/interfaces/annouce";
 import dataCar from "../../../dataTeste";
+import { useModal } from "@/contexts/modalContext";
 
 const CreateAnnouncementModal = () => {
   const { allCars, getAllCars, allBrands } = useAnnouncement();
-  const [selectedBrand, setSelectedBrand] = useState("" as any);
+  const { isOpen, onClose, onOpen } = useModal();
+  const [selectedBrand, setSelectedBrand] = useState("chevrolet" as any);
   const allCarsArr = [allCars];
   const allSelectedBrandsCars = [allCars[selectedBrand]];
-  // const [selectedCar, setSelectedCar] = useState("" as any);
-  // const fipeValue = dataCar.filter((car) => {
-  //   if (car.marca === selectedCar) {
-  //     return car.valor;
-  //   }
-  // });
+  const [selectedCar, setSelectedCar] = useState("" as any);
+  const [fipeValue, setFipeValue] = useState("" as any);
 
-  // console.log(fipeValue);
+  const fipeToFormatt = +fipeValue;
+  const formattedFipeValue = fipeToFormatt.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+
+  const getFipe = () => {
+    dataCar.filter((car) => {
+      if (car.modelo.toLowerCase() === selectedCar.toLowerCase()) {
+        setFipeValue(car.valor);
+      }
+    });
+  };
 
   return (
     <GeneralModal>
@@ -46,11 +58,18 @@ const CreateAnnouncementModal = () => {
           })}
         </Select>
         <FormLabel htmlFor="brand">Model</FormLabel>
-        <Select>
+        <Select
+          onChange={(e) => {
+            setSelectedCar(e.target.value), getFipe();
+          }}
+        >
           {allSelectedBrandsCars.map((car: any) => {
-            return car.map((element: any) => {
-              return <option key={element.name}>{element.name}</option>;
-            });
+            return (
+              car &&
+              car.map((element: any) => {
+                return <option key={element.name}>{element.name}</option>;
+              })
+            );
           })}
         </Select>
         <Box>
@@ -66,10 +85,30 @@ const CreateAnnouncementModal = () => {
           <Input placeholder="Branco" />
         </Box>
         <Box>
-          <FormLabel htmlFor="year">Ano</FormLabel>
-          <Input placeholder="2018" />
+          <FormLabel htmlFor="fipe">Preço tabela FIPE</FormLabel>
+          <Input
+            placeholder="R$ 48.000,00"
+            defaultValue={formattedFipeValue}
+            isDisabled={true}
+          />
           <FormLabel htmlFor="price">Preço</FormLabel>
           <Input placeholder="R$ 50.000,00" />
+        </Box>
+        <FormLabel htmlFor="description">Descrição</FormLabel>
+        <Textarea placeholder="Descrição..."></Textarea>
+
+        <FormLabel htmlFor="coverImg">Imagem de capa</FormLabel>
+        <Input placeholder="https://image.com" />
+
+        <FormLabel htmlFor="image1">1º Imagem da galeria</FormLabel>
+        <Input placeholder="https://image.com" />
+
+        <FormLabel htmlFor="image2">2º Imagem da galeria</FormLabel>
+        <Input placeholder="https://image.com" />
+
+        <Box>
+          <Button onClick={onClose}>Cancelar</Button>
+          <Button>Criar anuncio</Button>
         </Box>
       </FormControl>
     </GeneralModal>
