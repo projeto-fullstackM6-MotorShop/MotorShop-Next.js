2fbd2fc0835352d1ab9c8dd9921f13ab33110687
Igor <silveira.igor@outlook.com>
2023-04-24 11:27:09 -0300

    fix: fix some issues in auth context and login page

--- motorshop/src/interfaces/usersTypes.ts
+++ motorshop/src/interfaces/usersTypes.ts
@@ -1,26 +1,37 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
+export interface IUserData {
+  id: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  birth_date: string;
+  is_seller: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
 export interface IUserLogin {
-    email: string,
-    password: string
+  email: string;
+  password: string;
 }
 
 export interface IProviderProps {
-    children: ReactNode
+  children: ReactNode;
 }
 
 export interface IRegisterUser {
-    nome: string
-    email: string
-    cpf: string
-    phone: string
-    bornDate: Date
-    description: string
-    cep: string
-    state : string
-    city: string
-    addressComplement: string
-    typeAccount: string
-    number: string
-    password: string
+  nome: string;
+  email: string;
+  cpf: string;
+  phone: string;
+  bornDate: Date;
+  description: string;
+  cep: string;
+  state: string;
+  city: string;
+  addressComplement: string;
+  typeAccount: string;
+  number: string;
+  password: string;
 }
