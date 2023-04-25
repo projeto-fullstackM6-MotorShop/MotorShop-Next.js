4b5049815236ca796c22cb8468145a1980a6c202
Isaias Gregory <106548529+Lightgreg@users.noreply.github.com>
2023-04-25 18:25:47 -0300

    feat:createProfilePage

---
+++ motorshop/src/pages/profile.tsx
@@ -0,0 +1,64 @@
+import AnnouceCard from "@/components/annouceCard";
+import AvatarIcon from "@/components/avatarIcon";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { useAnnouncement } from "@/contexts/announcementContext";
+import { useAuth } from "@/contexts/authContext";
+import { IAnnouceInterface } from "@/interfaces/annouce";
+import { Box, Flex, Heading, SimpleGrid, Text } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+const Profile = () => {
+  const { user } = useAuth();
+  const router = useRouter();
+  const { allAnnouncements } = useAnnouncement()
+
+  const goForLogin = () => {
+    if (!user) {
+      router.push("/login");
+    }
+  };
+
+  return (
+    <>
+      <Header></Header>
+      <Box bgColor={"grey.8"} h={"1700px"} zIndex={1}>
+        <Box w={"100%"} h={"257px"} bgColor={"brand.1"} zIndex={-1} />
+        <Flex
+          position={"absolute"}
+          top={"180px"}
+          w={"100%"}
+          alignItems={'center'}
+          direction={"column"}
+        >
+          <Box w={'80%'} bgColor={'grey.10'} padding={'30px'} gap={'15px'} display={'flex'} flexDirection={'column'} borderRadius={'4px'}>
+
+            <AvatarIcon size="xl" />
+            <Flex gap={'10px'} alignItems={'center'}>
+              <Heading fontSize={"sm"}>Nome Qualquer</Heading>
+              <Text padding={"5px"}
+                bgColor={"brand.4"}
+                borderRadius={"4px"}
+                color={"brand.1"}>anunciante</Text>
+            </Flex>
+            <Text textAlign={"start"} color={"grey.3"}>
+              Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industries
+            </Text>
+          </Box>
+          <Heading mt={'25px'} mb={'30px'} w={'90%'} textAlign={'start'} fontSize={'lg'}>Anúncios</Heading>
+          <SimpleGrid columns={4} spacing={30} mt={'20px'} w={'90%'}>
+            {allAnnouncements.length > 0 ? (
+              allAnnouncements.map((data: IAnnouceInterface) =>
+                <AnnouceCard data={data} key={data.id} />)
+            ) : (<Text>Este usuario ainda nao possui anuncios</Text>)}
+          </SimpleGrid>
+
+        </Flex>
+      </Box>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default Profile
