826bf6f13ce3b2ec29ded61b8c462000e703b341
Igor <silveira.igor@outlook.com>
2023-05-03 23:41:45 -0300

    feat: add comment context

---
+++ motorshop/src/contexts/commentContext.tsx
@@ -0,0 +1,78 @@
+import { createContext, useContext, useState } from "react";
+import { IChildren } from "@/interfaces/misc";
+import { useAuth } from "./authContext";
+import { IComment, ICreateCommentData } from "@/interfaces/comments";
+import { api } from "@/services/api";
+
+interface CommentProviderData {
+  createComment: (data: ICreateCommentData, annoucementId: string) => void;
+  getAllCommentsOfAnnoucement: (annoucementId: string) => void;
+  deleteComment: (commentId: string, annoucementId: string) => void;
+  commentsOfAnnoucement: IComment[];
+}
+
+export const CommentContext = createContext<CommentProviderData>(
+  {} as CommentProviderData
+);
+
+export const CommentProvider = ({ children }: IChildren) => {
+  const [commentsOfAnnoucement, setCommentsOfAnnoucement] = useState<
+    IComment[]
+  >([]);
+
+  const { token } = useAuth();
+
+  const createComment = async (
+    data: ICreateCommentData,
+    annoucementId: string
+  ) => {
+    try {
+      const response = await api.post(`/comment/${annoucementId}`, data, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      getAllCommentsOfAnnoucement(response.data.id);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  const getAllCommentsOfAnnoucement = async (annoucementId: string) => {
+    try {
+      const response = await api.get(`/comment/${annoucementId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setCommentsOfAnnoucement(response.data);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  const deleteComment = async (commentId: string, annoucementId: string) => {
+    try {
+      await api.delete(`/comment/${commentId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      getAllCommentsOfAnnoucement(annoucementId);
+    } catch (error: any) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <CommentContext.Provider
+      value={{
+        createComment,
+        getAllCommentsOfAnnoucement,
+        deleteComment,
+        commentsOfAnnoucement,
+      }}
+    >
+      {children}
+    </CommentContext.Provider>
+  );
+};
+
+export const useComment = () => useContext(CommentContext);
---
+++ motorshop/src/interfaces/comments.ts
@@ -0,0 +1,9 @@
+export interface ICreateCommentData {
+  comment: string;
+}
+
+export interface IComment {
+  id: string;
+  comment: string;
+  createdAt: string;
+}
